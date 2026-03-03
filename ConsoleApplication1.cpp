5e29514f243776506b555a387cd749040bbc2ec3
nba0302 <nba-0302@ya.ru>
2026-03-03 22:44:04 +0500

    chrono

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,8 +1,16 @@
 ﻿#include <iostream>
-
+#include <chrono>
 int main()
 {
+    auto start = std::chrono::steady_clock::now();
     int g = 10;
-    g += 5;
+    g += 5 * 3 + 2;
     std::cout << "Hello 222World!\n";
+    for (int i = 0; i < 10000; i++) {
+        printf("%5i", i);
+    }
+    printf("\n");
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    std::cout << elapsed.count() << "\n";
 }
